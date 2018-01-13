3bd6ccf62657f873b2030691ba1dc1dfb3fef562
piczkaczu <35381537+piczkaczu@users.noreply.github.com>
2018-01-13 18:59:24 +0100

    Update compiled.js

--- compiled.js
+++ compiled.js
@@ -221,7 +221,7 @@ $("#primary-group").on("change", 'input[type="radio"]', function (e) {
       });
 
       // default
-      $("#secondary-group").prepend("\n        <label class=\"btn btn-secondary\" for=\"all\">\n          <input type=\"radio\" name=\"" + key + "\" id=\"all\" value=\"all\" checked>\n          Wszystkie\n        </label>\n      ");
+//      $("#secondary-group").prepend("\n        <label class=\"btn btn-secondary\" for=\"all\">\n          <input type=\"radio\" name=\"" + key + "\" id=\"all\" value=\"all\" checked>\n          Wszystkie\n        </label>\n      ");
       break;
     case "all":
       addToMap();
@@ -234,7 +234,7 @@ $("#primary-group").on("change", 'input[type="radio"]', function (e) {
       });
 
       // default
-      $("#secondary-group").append("\n        <label class=\"btn btn-light\" disabled>\n          Wykluczone na tydzien od:\n        </label>\n      ");
+//      $("#secondary-group").append("\n        <label class=\"btn btn-light\" disabled>\n          Wykluczone na tydzien od:\n        </label>\n      ");
 
       terrains.forEach(function (terrain) {
         $("#secondary-group").append("\n          <label class=\"btn btn-secondary\" for=\"" + terrain + "\">\n            <input type=\"radio\" name=\"" + key + "\" id=\"" + terrain + "\" value=\"" + terrain + "\"\n              " + (terrain === defaultButton ? "checked" : "") + ">\n            " + moment(terrain).format("DD-MM") + "\n          </label>\n        ");
